694e70d414a14f6b879da01fe7d64aa721fa3396
Jesse Sandhu <s.jesse23@gmail.com>
2023-10-07 04:44:54 -0700

    add sign up routes, user controller, user model and bundle/bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -75,4 +75,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
